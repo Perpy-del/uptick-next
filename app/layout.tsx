7df27ec8d9542aa1f9fde1036deea6b7e530e036
Perpy-del <perpetualmeninwa@gmail.com>
2024-07-24 20:39:21 +0100

    feat: :sparkles: complete the homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Playfair_Display, Roboto } from 'next/font/google'
 import './globals.css';
 import { ThemeProvider } from '../components/theme-provider';
 import NavBar from '@/components/NavBar';
+import { ModeToggle } from '@/components/ModeToggle';
 
 const playfair = Playfair_Display({
   subsets: ['latin'],
@@ -35,8 +36,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <NavBar />
           {children}
+          <ModeToggle />
         </ThemeProvider>
       </body>
     </html>
